84268887440891b604762025392450d82fe72793
Sujan Kandeepan <kandeeps@mcmaster.ca>
2020-09-22 16:41:44 -0700

    Added functionality to paint.cpp

--- A1/paint.cpp
+++ A1/paint.cpp
@@ -1,6 +1,7 @@
 // Standard C++ library imports
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <vector>
 
 // OpenGL and GLUT imports
@@ -31,7 +32,7 @@ struct point {
 	float pointX;
 	float pointY;
 	color pointColor;
-	float pointSize;
+	int pointSize;
 	point(){}
 };
 
@@ -53,14 +54,16 @@ static color availablePointColors[5] = {
 	color(0.000, 0.749, 1.000)  // deep sky blue
 };
 
-// Static value of current background color
-static color currentBackgroundColor = availableBackgroundColors[0];
+// Static index of current background color
+static int currentBackgroundColor = 0;
 
-// Static value of current brush color
-static color currentPointColor = availablePointColors[0];
+// Static index of current brush color
+static int currentPointColor = 0;
 
 // Static value of current point size (1-5)
-static float currentPointSize = 3;
+static int currentPointSize = 3;
+
+static char displayMessage[64] = { '\0' };
 
 // Static global array of points on screen
 static std::vector<point> drawnPoints;
@@ -78,9 +81,14 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Set background color before drawing points
-	glClearColor(currentBackgroundColor.value[0],
-		currentBackgroundColor.value[1],
-		currentBackgroundColor.value[2], 0);
+	glClearColor(
+		availableBackgroundColors[
+			currentBackgroundColor].value[0],
+		availableBackgroundColors[
+			currentBackgroundColor].value[1],
+		availableBackgroundColors[
+			currentBackgroundColor].value[2],
+		0);
 
 	// Iterate over all stored points to redraw
 	for (int i = 0; i < drawnPoints.size(); i++)
@@ -93,6 +101,21 @@ void display()
 			drawnPoints.at(i).pointY);
 		glEnd();
 	}
+
+	// Display text for point size/color and background color
+	glColor3fv(availablePointColors[0].value);
+	glRasterPos2f(5, DISPLAY_HEIGHT - 15);
+	memset(displayMessage, '\0', sizeof(displayMessage));
+	snprintf(displayMessage, sizeof(displayMessage),
+		"Point size: %d     "
+		"Point color: %d     "
+		"Background color: %d",
+		currentPointSize,
+		currentPointColor + 1,
+		currentBackgroundColor + 1);
+	for (int i = 0; i < strlen(displayMessage); i++)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,
+			displayMessage[i]);
 
 	// Flushes buffered commands to display
 	glFlush();
@@ -113,8 +136,7 @@ void keyboard(unsigned char key, int x, int y)
 		case '3':
 		case '4':
 		case '5':
-			currentPointColor =
-				availablePointColors[key - '1'];
+			currentPointColor = key - '1';
 			break;
 		// Change background colors
 		case '6':
@@ -122,9 +144,8 @@ void keyboard(unsigned char key, int x, int y)
 		case '8':
 		case '9':
 		case '0':
-			currentBackgroundColor =
-				availableBackgroundColors[key - '6'
-					+ (key == '0' ? 10 : 0)];
+			currentBackgroundColor = key - '6'
+				+ (key == '0' ? 10 : 0);
 			break;
 		// Increment point size (highest is 5)
 		case '+':
@@ -156,8 +177,12 @@ void keyboard(unsigned char key, int x, int y)
 // Mouse function: registers points during mouse click
 void mouse(int button, int state, int x, int y)
 {
-	// Adjust inverted y coordinate 
+	// Adjust inverted y coordinate
 	y = DISPLAY_HEIGHT - y;
+
+	// Refuse points anywhere along display text
+	if (y > DISPLAY_HEIGHT - 25 && x < 310)
+		return;
 
 	// Create and store point on left mouse click
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
@@ -166,7 +191,8 @@ void mouse(int button, int state, int x, int y)
 		point tempPoint;
 		tempPoint.pointX = (float)x;
 		tempPoint.pointY = (float)y;
-		tempPoint.pointColor = currentPointColor;
+		tempPoint.pointColor =
+			availablePointColors[currentPointColor];
 		tempPoint.pointSize = currentPointSize;
 		drawnPoints.push_back(tempPoint);
 	}
@@ -177,6 +203,23 @@ void motion(int x, int y)
 {
 	// Re-use logic in mouse function
 	mouse(GLUT_LEFT_BUTTON, GLUT_DOWN, x, y);
+}
+
+// Reshape function: reframes canvas upon window resize
+void reshape(int w, int h)
+{
+	// Update global constants for window dimensions
+	DISPLAY_WIDTH = w;
+	DISPLAY_HEIGHT = h;
+
+	// Reset pixel matrix with new dimensions
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, DISPLAY_WIDTH, 0, DISPLAY_HEIGHT);
+
+	// Reconstruct view of 2D projection
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
 }
 
 // Main function: entry point and initialization
@@ -196,20 +239,31 @@ int main(int argc, char ** argv)
 	glutInitWindowPosition(DISPLAY_OFFSET_WIDTH,
 		DISPLAY_OFFSET_HEIGHT);
 
+	// Display application controls in terminal
+	printf("Paint Application Controls:\n"
+		" - Left click and optionally drag "
+		"with mouse to draw points\n"
+		" - Numbers between '1' and '5' to "
+		"change point color\n"
+		" - Numbers between '6' and '0' to "
+		"change background color\n"
+		" - '+' to increment point size (highest is 5)\n"
+		" - '-' to decrement point size (lowest is 1)\n"
+		" - 'r' or 'R' to clear screen\n"
+		" - 'q' or 'Q' to quit application\n");
+
 	// Create window (main drawing canvas)
 	glutCreateWindow("3GC3 - Assignment 1");
 
 	// Make points into circles
 	glEnable(GL_POINT_SMOOTH);
 
-	// Set coordinate system to match window dimensions
-	gluOrtho2D(0, DISPLAY_WIDTH, 0, DISPLAY_HEIGHT);
-
 	// I/O function bindings
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
 	glutMouseFunc(mouse);
 	glutMotionFunc(motion);
+	glutReshapeFunc(reshape);
 
 	// Main program loop
 	glutMainLoop();
