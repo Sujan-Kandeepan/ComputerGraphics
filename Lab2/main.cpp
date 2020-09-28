a937f33751103f634a44ac2b0066cb7141b4033f
Sujan Kandeepan <kandeeps@mcmaster.ca>
2020-09-28 14:56:06 -0700

    Lab 2 submission

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -8,7 +8,10 @@
 #  include <GL/freeglut.h>
 #endif
 #include <iostream>
+#include <stdio.h>
 
+int pointX = 0, pointY = 0, offset = 0, pause = 0,
+	width = 800, height = 600;
 
 void drawPoint(int x, int y){
 	glPointSize(10);
@@ -16,76 +19,62 @@ void drawPoint(int x, int y){
 	glBegin(GL_POINTS);
         glVertex2f(x, y);
 	glEnd();
-
+	glFlush();
 	glutPostRedisplay();
 }
 
 void init() {  
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 	glutInitWindowPosition(0,0);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(width, height);
 	glutCreateWindow("3GC3 Lab 2");
 	glClearColor(0.0,0.0,0.0,0);
 	glColor3f(1.0,1.0,1.0);
     
-	gluOrtho2D(0,800,0,600);
+	gluOrtho2D(0,width,0,height);
 }
 
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	drawPoint(pointX, pointY);
+	drawPoint(pointX + offset, pointY);
+	drawPoint(pointX - offset, pointY);
+	drawPoint(pointX, pointY + offset);
+	drawPoint(pointX, pointY - offset);
 }
 
-/*Main*/
-int main (int argc,char** argv){
-	glutInit(&argc,argv);
-	init();
-	glutDisplayFunc(display);
-	/* Write your call backs here */
-	glutMainLoop();
-} 
-
-#ifdef __APPLE__
-#  include <OpenGL/gl.h>
-#  include <OpenGL/glu.h>
-#  include <GLUT/glut.h>
-#else
-#  include <GL/gl.h>
-#  include <GL/glu.h>
-#  include <GL/freeglut.h>
-#endif
-#include <iostream>
-using namespace std;
-
-float animateDistance = 0;
-float pointX = -999;
-float pointY = -999;
-
-/*This function will require modifications*/
-void drawPoint(int x, int y){
-	glPointSize(10);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glBegin(GL_POINTS);
-	glVertex2f(x, y);
-	glEnd();
-}
-
-void init() {  
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowPosition(0,0);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("3GC3 Lab 2");
-	glClearColor(0.0,0.0,0.0,0);
-	glColor3f(1.0,1.0,1.0);
-	gluOrtho2D(0,800,0,600);
-}
-
-/*This function will require modifications*/
-void display(void)
+void keyboard(unsigned char key, int x, int y)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
+	switch (key)
+	{
+		case 'p':
+		case 'P':
+			if (pause == 0)
+				pause = 1;
+			else if (pause == 1)
+				pause = 0;
+		break;
+	}
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	y = height - y;
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		pause = 0;
+		pointX = x;
+		pointY = y;
+		offset = 0;
+	}
+}
+void timer(int val)
+{
+	if (pause == 0	)
+		offset += 10;
+	glutTimerFunc(20, timer, 0);
 }
 
 /*Main*/
@@ -94,5 +83,8 @@ int main (int argc,char** argv){
 	init();
 	glutDisplayFunc(display);
 	/* Write your call backs here */
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
+	glutTimerFunc(20, timer, 0);
 	glutMainLoop();
 } 
