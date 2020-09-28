5d71f772ff1d91081b4e1b1d08fecbb631ab7cef
Sujan Kandeepan <kandeeps@mcmaster.ca>
2020-09-28 15:17:53 -0700

    Bonus features and small adjustments for Lab 2

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -11,11 +11,12 @@
 #include <stdio.h>
 
 int pointX = 0, pointY = 0, offset = 0, pause = 0,
-	width = 800, height = 600;
+	rainbow = 0, width = 800, height = 600;
+
+float colours[5][3];
 
 void drawPoint(int x, int y){
 	glPointSize(10);
-	glColor3f(1.0f, 1.0f, 1.0f);
 	glBegin(GL_POINTS);
         glVertex2f(x, y);
 	glEnd();
@@ -37,10 +38,15 @@ void init() {
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3fv(colours[0]);
 	drawPoint(pointX, pointY);
+	glColor3fv(colours[1]);
 	drawPoint(pointX + offset, pointY);
+	glColor3fv(colours[2]);
 	drawPoint(pointX - offset, pointY);
+	glColor3fv(colours[3]);
 	drawPoint(pointX, pointY + offset);
+	glColor3fv(colours[4]);
 	drawPoint(pointX, pointY - offset);
 }
 
@@ -54,6 +60,17 @@ void keyboard(unsigned char key, int x, int y)
 				pause = 1;
 			else if (pause == 1)
 				pause = 0;
+			break;
+		case 'q':
+		case 'Q':
+			exit(0);
+			break;
+		case ' ':
+			if (rainbow == 0)
+				rainbow = 1;
+			else if (rainbow == 1)
+				rainbow = 0;
+			break;
 		break;
 	}
 }
@@ -64,6 +81,18 @@ void mouse(int button, int state, int x, int y)
 
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
 	{
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (rainbow == 1)
+					colours[i][j] = ((float) rand()) / (float) RAND_MAX;
+				else
+					colours[i][j] = 1.0f;
+			}
+		}
+		printf("%.3f %.3f %.3f\n", colours[0][0], colours[0][1], colours[0][2]);
+
 		pause = 0;
 		pointX = x;
 		pointY = y;
@@ -73,7 +102,7 @@ void mouse(int button, int state, int x, int y)
 void timer(int val)
 {
 	if (pause == 0	)
-		offset += 10;
+		offset += 5;
 	glutTimerFunc(20, timer, 0);
 }
 
