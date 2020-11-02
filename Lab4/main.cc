060f9f01629f0b838c925eb46fd91b6127104b3d
Trandinh Thien <trandit@mcmaster.ca>
2020-11-02 16:23:21 -0500

    Added lab4 + boilerplate code

---
+++ Lab4/main.cc
@@ -0,0 +1,72 @@
+#ifdef __APPLE__
+#  define GL_SILENCE_DEPRECATION
+#  include <OpenGL/gl.h>
+#  include <OpenGL/glu.h>
+#  include <GLUT/glut.h>
+#else
+#  include <GL/gl.h>
+#  include <GL/glu.h>
+#  include <GL/freeglut.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+GLdouble eye[] = { 0, 0, 10 };
+GLdouble lookAt[] = { 0, 0, 0 };
+GLdouble up[] = { 0, 1, 0 };
+
+
+
+void display(void) {
+ 
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(
+        eye[0], eye[1], eye[2],
+        lookAt[0], lookAt[1], lookAt[2],
+        up[0], up[1], up[2]
+    );
+
+   
+    glutSolidSphere(2, 100, 100);
+
+    glFlush();
+}
+
+void handleReshape(int w, int h) {
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45, 1, 1, 100);
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void handleKeyboard(unsigned char key, int _x, int _y) {
+    if (key == 'q') {
+        exit(0);
+    }
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitWindowSize(600,600);
+    glutInitWindowPosition(300,300);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("Tutorial 4");
+
+    glutKeyboardFunc(handleKeyboard);
+    glutReshapeFunc(handleReshape);
+    glutDisplayFunc(display);
+
+    
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glShadeModel(GL_SMOOTH);
+
+    glutMainLoop();
+
+    return 0;
+}
